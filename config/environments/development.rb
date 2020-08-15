0e70bb00291e07ea30eb01cf08b97723c2713804
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-15 14:25:30 +0900

    deviseに関するビュー、メール認証機能の実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV["GMAIL_USER"], #gmailアドレス
+    :password => ENV["GMAIL_PASSWORD"], #gmailパスワード
+    :authentication => 'login',
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
