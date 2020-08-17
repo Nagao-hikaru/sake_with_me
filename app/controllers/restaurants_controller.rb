bf9385791cb8bcef3e46bf14f9b58f0ad163e498
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-17 14:18:12 +0900

    new,createアクション,ストロングパラメータの設定

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,5 +3,24 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurnt.create
+      redirect_to root_path, notice: '投稿に成功しました。'
+    else
+      flash[:alert] = "全項目記述してください"
+      render :new
+    end
+
+  end
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :image, :beer, :genre_id, :prefecture_id, :city, :address).merge(user_id: current_user.id)
   end
 end
