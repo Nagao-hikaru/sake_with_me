909b0663826522b36aef8b88028bcce5bf9740eb
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-17 12:31:14 +0900

    バリデーションの設定

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,21 @@ class Restaurant < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
   belongs_to_active_hash :prefecture
-
-
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :beer
+    validates :genre
+    validates :prefecture
+    validates :city
+    validates :address
+  end
+
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :genre_id
+    validates :prefecture_id
+  end
 end
