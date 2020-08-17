f0d4c820f46da5e408da956cc82b6765f2ea1a51
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-17 12:18:03 +0900

    アソシエーションの定義

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :prefecture
+
+
+  belongs_to :user
+  has_one_attached :image
 end
