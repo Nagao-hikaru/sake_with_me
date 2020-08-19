3ea63e035fb799d15b35765d01185aaf45216e01
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-19 11:24:55 +0900

    飲食店showページ作成

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new]
+  before_action :set_restaurant, only: [:show]
 
 
   def index
@@ -19,7 +20,10 @@ class RestaurantsController < ApplicationController
       flash[:alert] = "全項目記述してください"
       render :new
     end
+  end
 
+  def show
+    
   end
 
 
@@ -27,5 +31,9 @@ class RestaurantsController < ApplicationController
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :image, :beer, :genre_id, :prefecture_id, :city, :address).merge(user_id: current_user.id)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
