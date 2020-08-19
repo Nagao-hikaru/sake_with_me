be550768dc18a420e649bf646deab4b79b6911d9
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-19 12:17:07 +0900

    削除機能、条件分岐、ユーザーのページ遷移禁止機能の実装

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,8 @@
 class RestaurantsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
-  before_action :set_restaurant, only: [:show]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destory]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :forbit_restaurant, only: [:edit, :update, :destroy]
 
 
   def index
@@ -23,7 +24,26 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant.id), notice: '編集に成功しました。'
+    else
+      flash[:alert] = "全項目記述してください"
+      render :new
+    end
+  end
+
+  def destroy
+    if @restaurant.destroy
+      redirect_to root_path, notice: '削除に成功しました。'
+    else
+      render :edit
+    end
   end
 
 
@@ -35,5 +55,12 @@ class RestaurantsController < ApplicationController
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def forbit_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    if current_user != @restaurant.user.id
+      redirect_to restaurant_path(@restaurant.id), notice: '投稿者のみ編集,削除できます。'
+    end
   end
 end
