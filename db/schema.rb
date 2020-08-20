18338057d3adc303e211296f87ba578507f202e2
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-20 16:56:30 +0900

    データベース作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_030424) do
+ActiveRecord::Schema.define(version: 2020_08_20_064008) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,20 @@ ActiveRecord::Schema.define(version: 2020_08_17_030424) do
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
+  create_table "sakes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "rice_rate", null: false
+    t.string "sake_degree", null: false
+    t.integer "type_id", null: false
+    t.integer "degree", null: false
+    t.string "company", null: false
+    t.string "rice"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sakes_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,4 +79,5 @@ ActiveRecord::Schema.define(version: 2020_08_17_030424) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "restaurants", "users"
+  add_foreign_key "sakes", "users"
 end
