07df0000c150d61757cacfed86a8112d50a6c164
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-20 16:56:45 +0900

    アソシエーション、バリデーション記述

--- app/models/sake.rb
+++ app/models/sake.rb
@@ -1,2 +1,21 @@
 class Sake < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :type
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :nema
+    validates :rice_rate
+    validates :sake_degree
+    validates :type
+    validates :degree
+    validates :company
+    validates :rice
+  end
+
+  with_options numericality: { other_than: 1, message: 'を選択してください'} do
+    validates :type_id
+  end
 end
