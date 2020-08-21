17df6c4f294c05fb7ff294a2dd01673510a0f6d5
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-21 14:45:39 +0900

    バリデーション修正、ビューの調整

--- app/models/sake.rb
+++ app/models/sake.rb
@@ -4,6 +4,11 @@ class Sake < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+
+  # DEGREE_REGEX = /\A([1-9]\d*|0)\z/
+  RICE_RATE_REGEX = /\A[+-]/
+  
+
   with_options presence: true do
     validates :image
     validates :name
@@ -14,7 +19,17 @@ class Sake < ApplicationRecord
     validates :company
   end
 
+  validates :sake_degree, format: { with: RICE_RATE_REGEX, message: 'は最初に+か-をつけてください'}
+
+  validates :degree, numericality: { only_integer: true, grater_than: 0, less_than: 21, message: 'は21以下で入力してください'}
+
+
   with_options numericality: { other_than: 1, message: 'を選択してください'} do
     validates :type_id
+  end
+
+  def degree=(value)
+    value.tr!('０-９', '0-9') if value.is_a?(String)
+    super(value)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_064008) do
+ActiveRecord::Schema.define(version: 2020_08_21_025904) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
