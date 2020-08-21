6aac5b301b870ab9f819051e34054a2d196ba365
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-21 17:08:15 +0900

    中間テーブルの作成

---
+++ app/models/sake_restaurant.rb
@@ -0,0 +1,2 @@
+class SakeRestaurant < ApplicationRecord
+end
---
+++ db/migrate/20200821080440_create_sake_restaurants.rb
@@ -0,0 +1,9 @@
+class CreateSakeRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :sake_restaurants do |t|
+      t.references :sake, null: false, index: true, foreign_key: true
+      t.references :restaurant, null: false, index: true, foreign_key: true
+      t.timestamps
+    end
+  end
+end
