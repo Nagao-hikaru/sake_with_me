840f6081c1d06fc7266b96700357a66662757913
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-25 17:32:23 +0900

    jsの新規読み込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../hover")
+require("../sake_restaurant")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
