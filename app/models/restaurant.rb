c839f9312371505925d0abe5ca24ed2cff000100
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-26 14:09:43 +0900

    geocodeの使用設定

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,6 +6,7 @@ class Restaurant < ApplicationRecord
   has_one_attached :image
   has_many :sake_restaurants, dependent: :destroy
   has_many :sakes, through: :sake_restaurants, dependent: :destroy
+  geocoded_by :address
 
   with_options presence: true do
     validates :image
