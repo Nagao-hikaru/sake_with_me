0b3178cbdb35441c15f1eb80d9bed99b5dd4b0ec
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-26 12:43:25 +0900

    緯度経度カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_080440) do
+ActiveRecord::Schema.define(version: 2020_08_26_034014) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_08_21_080440) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
