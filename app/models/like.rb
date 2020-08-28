05d8c376cbf55e2c878669ce66b35f8f0e3aa94b
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-28 14:09:49 +0900

    likeモデルバリデーション設定

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
+
+  validates :user_id, presence: true, uniqueness: {scope: :restaurant_id}
+  validates :restaurant_id, presence: true
 end
