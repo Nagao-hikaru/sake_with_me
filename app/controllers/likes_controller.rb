c5779e622f77485d617ef5e10672773d10a18150
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-30 09:37:13 +0900

    部分テンプレート作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,11 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.create(restaurant_id: params[:restaurant_id])
-    redirect_to root_path
   end
 
   def destroy
     @like = Like.find_by(restaurant_id: params[:restaurant_id], user_id: current_user.id)
     @like.destroy
-    redirect_to root_path
     
   end
 
