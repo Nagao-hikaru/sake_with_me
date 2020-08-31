e9e8e4d6587910ac771ea45ca85f2749acfacbe8
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-31 16:47:49 +0900

    修正

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,11 +1,10 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )){
 
   document.addEventListener('DOMContentLoaded', function(){
-    console.log('123')
+
     const imageList = document.getElementById('image-list');
-    console.log(imageList)
-    document.getElementById('sake_image').addEventListener('change',function(e){
-      console.log(e)
+
+    document.getElementById('restaurant_image').addEventListener('change',function(e){
 
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
