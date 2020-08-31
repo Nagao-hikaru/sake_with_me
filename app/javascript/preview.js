b513b47abf9ee33e7f19ac6b6b190e71c76abd68
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-08-31 14:28:29 +0900

    飲食店投稿にプレビュー機能実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,7 +1,24 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )){
 
   document.addEventListener('DOMContentLoaded', function(){
-    
+    console.log('123')
+    const imageList = document.getElementById('image-list');
+    console.log(imageList)
+    document.getElementById('restaurant_image').addEventListener('change',function(e){
+      console.log(e)
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      console.log(blob)
+
+      const imageElement = document.createElement('div');
+
+      const blobImage = document.createElement('img');
+
+      blobImage.setAttribute('src', blob);
+      imageList.appendChild(imageElement);
+      imageElement.appendChild(blobImage);
+    });
   });
 
 }
