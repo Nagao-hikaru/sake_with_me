c8e886f1ee7c6f12e141fd61ad7e590f2443f71f
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-09-02 21:02:09 +0900

    js 設定

--- app/javascript/tab.js
+++ app/javascript/tab.js
@@ -1,14 +1,24 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//   console.log('aaa')
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('aaa')
 
-//   const menuItems = document.querySelectorAll('.user-menu li a')
-//   console.log(menuItems)
+  const menuItems = document.querySelectorAll('.user-menu li a')
+  const contents = document.querySelectorAll('.content')
 
-//   menuItems.forEach(item => {
-//     item.addEventListener('click', e => {
-//       e.preventDefault();
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+      e.preventDefault();
+      // preventDefaultでリンク先のページ遷移を防いでいる。
+      menuItems.forEach( item => {
+        item.classList.remove('active')
+      })
 
-//       item.classList.add('active');
-//     })
-//   })
-// })
+      clickedItem.classList.add('active');
+
+      contents.forEach(content => {
+        content.classList.remove('active');
+      })
+      
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
+    });
+  });
+})
