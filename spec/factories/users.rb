7eb495e709e05d065cee3107f8488add6512811b
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-09-06 12:12:56 +0900

    factorybot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name {Faker::Name.name}
     email {Faker::Internet.free_email(name: 'hikaru')}
-    password {Faler::Internet.password(min_length: 6)}
+    password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     
   end
