230600e759969a149881c160f3a95cf95bcd0ecd
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-09-07 15:01:17 +0900

    日本酒factorybot作成

--- spec/factories/sakes.rb
+++ spec/factories/sakes.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :sake do
-    
+    name {'特別純米酒 山田錦'}
+    rice_rate {'70%'}
+    sake_degree {-20}
+    type_id {Faker::Number.between(from: 2, to: 11)}
+    degree {15}
+    company {Faker::Company.name}
+    rice {'山田錦'}
+    association :user
   end
 end
--- spec/models/sake_spec.rb
+++ spec/models/sake_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe Sake, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @sake = FactoryBot.build(:sake)
+    @sake.image = fixture_file_upload('public/images/test_image.png')
+  end
+
+  describe '日本酒投稿' do
+    context '日本酒投稿がうまくいく時' do
+      it '全ての項目を正しく記入していれば登録できる' do
+        expect(@sake).to be_valid 
+      end
+      it '使用米を記入しなくても登録できる' do
+        @sake.rice = nil
+        expect(@sake).to be_valid
+      end
+      it '精米歩合の最後に%がついていると登録できる' do
+        @sake.rice_rate = '25%'
+        expect(@sake).to be_valid
+      end
+      it '日本酒度の最初に-か+がついていたら登録できる' do
+        @sake.sake_degree = -20
+        expect(@sake).to be_valid
+      end
+      it 'アルコール度数は21度以下なら登録できる' do
+        @sake.degree = 20
+        expect(@sake).to be_valid
+      end
+      it 'アルコール度数は文字列でもint型になり登録できる。' do
+        @sake.degree = "19"
+        expect(@sake).to be_valid
+      end
+    end
+
+    context '日本酒投稿がうまくいかない時' do
+      it '日本酒名が空だと登録できない' do
+        @sake.name = nil
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include("日本酒名を入力してください")
+      end
+      it '画像が挿入されていないと登録できない' do
+        @sake.image = nil
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('画像を入力してください')
+      end
+      it '精米歩合が空だと登録できない' do
+        @sake.rice_rate = nil
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('精米歩合を入力してください')
+      end
+      it '精米歩合の最後に%がつかなければ登録できない' do
+        @sake.rice_rate = "20"
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('精米歩合は最後に%を記入してください')
+      end
+      it '日本酒度が空だと登録できない' do
+        @sake.sake_degree = nil
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('日本酒度を入力してください')
+      end
+      it '日本酒度は最初に+か-を記入しないと登録できない' do
+        @sake.sake_degree = 23
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('日本酒度は最初に+か-を記入してください')
+      end
+      it '種類を選択しないと登録できない' do
+        @sake.type_id = 1
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('種類を選択してください')
+      end
+      it 'アルコール度数が空だと登録できない' do
+        @sake.degree = nil
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('アルコール度数を入力してください')
+      end
+      it 'アルコール度数が22度以上だと登録できない' do
+        @sake.degree = 22
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('アルコール度数は21以下で入力してください')
+      end
+      it '酒蔵が空だと登録できない' do
+        @sake.company = nil
+        @sake.valid?
+        expect(@sake.errors.full_messages).to include('酒蔵を入力してください')
+      end
+    end
+  end
 end
