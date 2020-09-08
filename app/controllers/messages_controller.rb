d472758608f2ca5388c4601a284fb3992e9ddadb
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-09-08 20:22:11 +0900

    ストロングパラメータ設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,15 @@
 class MessagesController < ApplicationController
 
   def create
-    @message = Message.new(text: params[:message][:text])
+    @message = Message.new(message_params)
+    if @message.save
+      ActionCable.server.broadcast 'message_channel', content: @message
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id)
   end
 end
